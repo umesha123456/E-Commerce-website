daab31e9b8418af0e06a83166182743e4181b8a6
S Brinthapan <brinthapans@esn.ac.lk>
2025-07-03 21:04:28 +0530

    Initial Commit

---
+++ ecommerce-backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: 'Unauthorized: No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId; // Attach user ID to the request
+    next();
+  } catch (err) {
+    console.error('Token verification failed:', err);
+    res.status(401).json({ message: 'Unauthorized: Invalid token' });
+  }
+}
+
+module.exports = authMiddleware;
