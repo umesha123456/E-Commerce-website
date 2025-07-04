5c649196e34e904778d4689d6a543cb4ae39536f
S Brinthapan <brinthapans@esn.ac.lk>
2025-07-04 14:50:42 +0530

    Empty the bracnch

--- BackEnd/product-api/models/Product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-  name: String,
-  status: { type: String, enum: ['published', 'not published'], required: true }
-});
-
-module.exports = mongoose.model('Product', productSchema);
--- FrontEnd/src/app/cart/components/cart-product-loading/cart-product-loading.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cart-product-loading',
-  templateUrl: './cart-product-loading.component.html',
-})
-export class CartProductLoadingComponent {}
