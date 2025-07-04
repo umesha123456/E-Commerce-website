dea08353115714b3f1f120f549f0ff90bd52ff28
S Brinthapan <brinthapans@esn.ac.lk>
2025-07-04 10:30:29 +0530

    empty the branch

--- FrontEnd/src/app/shared/components/navbar/navbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-navbar',
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html',
-})
-export class NavbarComponent {}
--- FrontEnd/src/app/shared/models/product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-  id: string;
-  name: string;
-  description: string;
-  urlImg: string;
-  reviews: number;
-  price: number;
-  previousPrice: number | null;
-}
